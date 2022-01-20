0143dc098369e1cbd418f22a08c89f094ae6bd36
Thiago Soares <thiagosantana12@gmail.com>
2022-01-20 17:43:17 -0300

    Envio do arquivo firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDKho-Z4b4QTqzCCpFCXdb8-Ung70w9_cY",
+  authDomain: "todolist-9b45f.firebaseapp.com",
+  projectId: "todolist-9b45f",
+  storageBucket: "todolist-9b45f.appspot.com",
+  messagingSenderId: "745273034799",
+  appId: "1:745273034799:web:596c462dfe70d6b632deda"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
